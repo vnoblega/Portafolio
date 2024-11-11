5a2a939dacd1693506a03dbc1a70a2130fe53a3d
Noblega Victor <27932054@alumnos.frsr.utn.edu.ar>
2024-11-11 17:28:46 -0300

    Subir portafolio inicial

---
+++ src/components/ExperienciaComponente.vue
@@ -0,0 +1,62 @@
+<script setup>
+import icono1 from '/src/assets/ventas.svg';
+import icono2 from '/src/assets/ecomerce.svg';
+import icono3 from '/src/assets/configuracion.svg';
+import icono4 from '/src/assets/uiux.svg';
+import icono5 from '/src/assets/watch.svg';
+
+import { ref } from 'vue';
+const titulo = 'Tecnico en Programación';
+const fecha = '2024';
+const experiencias = ref([
+    { id: 1, src: icono1, parrafo: 'Creación repositorio' },
+]);
+</script>
+
+<template>
+    <div class="card">
+        <h3 class="titulo">{{ titulo.toLocaleUpperCase() }}</h3>
+        <p class="fecha">{{ fecha }}</p>
+        <ul class="listado">
+            <li class="item" v-for="experencia in experiencias" :key="experencia.id">
+                <img class="imagen-svg" :src="experencia.src" width="45rem" :alt="experencia.parrafo">
+                <p>{{ experencia.parrafo }}</p>
+            </li>
+
+
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    display: flex;
+    flex-direction: column;
+    padding: 2rem;
+    background-color: rgb(28, 41, 52);
+    border-radius: 15px;
+}
+
+.titulo {
+    font-size: 1.5rem;
+    color: coral;
+}
+
+.fecha {
+    font-size: 1rem;
+    color: burlywood;
+    margin-bottom: 1rem;
+}
+
+.listado {
+    display: flex;
+    flex-direction: column;
+}
+
+.item {
+    align-items: center;
+    display: flex;
+    padding: 1rem;
+    gap: 1.5rem;
+}
+</style>
